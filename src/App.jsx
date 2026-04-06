5d732c0e6987385da00b0eb2a7df3d3af97999ab
Sai Sumiran <164289250+Sumiran2105@users.noreply.github.com>
2026-04-06 16:23:27 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-black text-white">
+      Tailwind v3 is working 🚀
+    </div>
+  );
+}
